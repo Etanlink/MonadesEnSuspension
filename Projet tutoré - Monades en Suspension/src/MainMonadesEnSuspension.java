152d034ce0931daa9ceb03086c672a37adce55a0
Etanlink <Etanlink@gmail.com>
2015-01-30 19:19:17 +0100

    Fix of a error in the mainMonadesEnSuspension class

--- Projet tutoré - Monades en Suspension/src/MainMonadesEnSuspension.java
+++ Projet tutoré - Monades en Suspension/src/MainMonadesEnSuspension.java
@@ -1,12 +1,3 @@
-/**
- * @aim Launch the application
- * 
- * @date 30/01/2015
- * @author Etanlink
- * @version 0.0
- *
- **/
-package controller;
 
 import java.io.IOException;
 
@@ -17,37 +8,30 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainMonadesEnSuspension extends Application {
-	
-	/* Creation of the MainStage */
+
 	private Stage primaryStage;
 	private BorderPane UIView;
 	
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
-		this.primaryStage.setTitle("Monades En Suspension");
+		this.primaryStage.setTitle("MonadesEnSuspension");
 		
-		initUIView();
-	}
-	
-	/**
-     * Initializes the UIView.
-     */
-    public void initUIView() {
-        try {
-            // Load UIView from fxml file.
+		try {
+            // Load root layout from fxml file.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainMonadesEnSuspension.class.getResource("view/RootLayout.fxml"));
+            loader.getLocation(MainMonadesEnSuspension.class.getResource("view/UIView.fxml"));
             UIView = (BorderPane) loader.load();
 
-            // Show the scene containing the UIView.
+            // Show the scene containing the root layout.
             Scene scene = new Scene(UIView);
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
+		
+	}
 
 	public static void main(String[] args) {
 		launch(args);
