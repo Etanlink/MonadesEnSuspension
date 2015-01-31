f451bbb3319898a18637ecaf4bcd4f344e6d7aac
Etanlink <Etanlink@gmail.com>
2015-01-31 09:08:30 +0100

    Add of class description in Axe class

--- Projet tutoré - Monades en Suspension/src/model/Axe.java
+++ Projet tutoré - Monades en Suspension/src/model/Axe.java
@@ -1,5 +1,14 @@
 package model;
 
+/**
+ * @aim Define an axe and attribute it at a Monade object 
+ * 
+ * @date 31/01/2015
+ * @author HugoPopo, Etanlink
+ * @version 0.0
+ *
+ **/
+
 public class Axe {
 
 }
