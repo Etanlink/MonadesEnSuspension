60709c6ddf8d2a4f83a630484c2f769405342da8
Nathan <Nathan@popy>
2015-04-03 00:02:38 +0200

    Adding two classes for the Interfaces !

---
+++ Projet tutoré - Monades en Suspension/src3/adress/MainApp2.java
@@ -0,0 +1,31 @@
+package adress;
+
+import adress.model.WindowImpl;
+import adress.model.WindowImpl2;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp2 extends Application{
+
+	  public Stage primaryStage;
+	    public WindowImpl2 Window;
+	    
+		public static void main(String[] args) {
+			launch(args);
+		}
+
+		@Override
+		public void start(Stage primaryStage) throws Exception {
+			
+			/* Instantiation of a new window */
+			this.primaryStage = primaryStage ;
+			WindowImpl2 window = new WindowImpl2();
+			this.Window = window;
+			this.Window.primaryStageInitialisation(primaryStage);
+			this.Window.setMainApp(this);
+		}
+
+
+
+
+}
---
+++ Projet tutoré - Monades en Suspension/src3/adress/model/WindowImpl2.java
@@ -0,0 +1,423 @@
+package adress.model;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.controlsfx.dialog.Dialogs;
+
+import com.sun.javafx.scene.accessibility.Action;
+
+import adress.MainApp2;
+import adress.MainTestCatchWindow;
+import adress.MainTestIHM;
+import javafx.animation.Animation;
+import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class WindowImpl2 {
+
+	private Stage primaryStage;
+
+	//private BorderPane rootLayout;
+
+	private Group root = new Group();
+	
+	public AnimationImpl animation;
+	
+	public boolean animationRunning = false;
+
+	public static final int SCENE_SIZE = 600;
+	
+	public static final int W_SCENE_SIZE = 800;
+	public static final int H_SCENE_SIZE = 600;
+	private MainApp2 MainApp;
+	public VBox MaVBox ;
+	private Rectangle snapshotRectangle = new Rectangle(225, 25, 585, 600);
+	
+	private static final int PARAMETERS_PANE_SIZE = 620;
+	
+	@FXML
+	public Button BoutonLancerAnim ;
+	
+	@FXML
+	public TextField textField1 ;
+	
+	@FXML
+	public TextField textField2 ;
+	
+	@FXML
+	public TextField textField3 ;
+	
+	@FXML
+	public TextField textField4 ;
+	
+	@FXML
+	public Slider Slider1;
+	
+	@FXML
+	public Slider Slider2;
+	
+	@FXML
+	public Slider Slider3;
+	
+	@FXML
+	public Slider Slider4;
+
+	public WindowImpl() {	}
+	/**
+	 * Initialize the primarystage
+	 */
+	public void primaryStageInitialisation(Stage primaryStage) throws IOException {
+		primaryStage.setTitle("Monades en suspension");
+		primaryStage.setResizable(false);
+		
+		this.animation = new AnimationImpl(this.root);
+
+		//MenuBar menuBar = loadMenuBar();
+		MenuBar menuBar = manualMenuBar();
+		
+		FXMLParametersVBox();
+
+		this.primaryStage = primaryStage;
+		
+		this.snapshotRectangle.setFill(Color.TRANSPARENT);
+		
+		Scene scene = new Scene(this.root, W_SCENE_SIZE, H_SCENE_SIZE);
+		primaryStage.setScene(scene);
+		/* the different parts of UI are bound AFTER the animation */
+		/*									   ^^^^^			   */
+		this.root.getChildren().add(this.MaVBox);
+		this.root.getChildren().add(menuBar);
+		this.root.getChildren().add(this.snapshotRectangle);
+		this.primaryStage.show();
+
+	}
+
+	/**
+	 * Creates a basic menuBar
+	 * @return MenuBar : the menuBar of the window
+	 */
+	public MenuBar manualMenuBar() {
+		final Menu helpMenuItem = new Menu("Aide");
+		helpMenuItem.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	
+            }
+        });
+		final Menu aboutMenuItem = new Menu("À propos");
+		aboutMenuItem.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	
+            }
+        });
+
+		MenuBar menuBar = new MenuBar();
+		menuBar.getMenus().addAll(helpMenuItem, aboutMenuItem);
+		menuBar.setMinWidth(W_SCENE_SIZE+10);
+		return menuBar;
+	}
+	
+	@FXML
+	public void initialize() {
+		
+	}
+	
+	
+	public void FXMLParametersVBox() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(WindowImpl.class.getResource("VBox.fxml"));
+			VBox vBox = (VBox)loader.load();
+			this.MaVBox = vBox ;
+			WindowImpl2 controller = loader.getController();
+			controller.setMainApp(this.MainApp);
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void setMainApp(MainApp2 mainapp){
+		this.MainApp = mainapp;
+	}
+	/**
+	 * Creates a basic parametersPane
+	 * @return parametersPane
+	 * @throws IOException 
+	 */
+	private VBox manualParametersVBox() throws IOException {
+		
+		final VBox parametersVBox = new VBox();
+		parametersVBox.setPrefSize(220, PARAMETERS_PANE_SIZE);
+		parametersVBox.setStyle("-fx-background-color: #f5f5f5;");
+		
+		/* Add of a VBox title */
+		Text VBoxTitle = new Text("Paramètres");
+		VBoxTitle.setFont(Font.font("System", FontWeight.SEMI_BOLD, 16));
+		parametersVBox.getChildren().add(VBoxTitle);
+		
+		/* Align all of the VBox components to CENTER_LEFT */
+		parametersVBox.setAlignment(Pos.BASELINE_CENTER);
+		/* Addition of spacing between the VBox Components */
+		parametersVBox.setSpacing(20);
+		//parametersVBox.setMargin((Node) parametersVBox.getChildren(),new Insets(20));
+		
+		/* Add of the different controllers */
+		Label LabelNBMinObjects = new Label("Nombre minimum de Monades");
+		Slider NbMinObjects = new Slider(3, 10, 3);
+		NbMinObjects.setShowTickLabels(true);
+		
+		Label LabelTinyObjectsPercentage = new Label("Pourcentage de petites Monades");
+		Slider TinyObjectsPercentage = new Slider(0, 100, 0);//respectively : min, max, beginValue
+		TinyObjectsPercentage.setShowTickLabels(true);
+		
+		Label LabelNormalObjectsPercentage = new Label("Pourcentage de moyennes de Monades");
+		Slider NormalObjectsPercentage = new Slider(0, 100, 0);//respectively : min, max, beginValue;
+		NormalObjectsPercentage.setShowTickLabels(true);
+		
+		Label LabelBigObjectsPercentage = new Label("Pourcentage de grandes Monades");
+		Slider BigObjectsPercentage = new Slider(0, 100, 0);//respectively : min, max, beginValue;
+		BigObjectsPercentage.setShowTickLabels(true);
+		
+		Button launchAnimationButton = new Button();
+		launchAnimationButton.setText("Lancer l'animation");
+		launchAnimationButton.setOnAction(new EventHandler<ActionEvent>() {
+			
+			
+		    @Override 
+		    public void handle(ActionEvent actionEvent) {
+		    	animationRunning = true;
+		        /* Get the different parameters specified by the controllers */
+		    	double getNbMinObjectsParameter = NbMinObjects.getValue();
+		    	double getTinyObjectsPercentage = TinyObjectsPercentage.getValue();
+		    	double getNormalObjectsPercentage = NormalObjectsPercentage.getValue();
+		    	double getBigObjectsPercentage = BigObjectsPercentage.getValue();
+		    	
+		    	/* Add of the animation controllers */
+		    	/* Launch the animation */
+		    	
+		    	//animation.animationWithParameters(getNbMinObjectsParameter, getTinyObjectsPercentage, getNormalObjectsPercentage, getBigObjectsPercentage);
+				animation.run();
+		    } 
+		});
+		
+		Button pauseAnimationButton = new Button();
+		pauseAnimationButton.setText("Mettre en pause l'animation");
+		pauseAnimationButton.setOnAction(new EventHandler<ActionEvent>() { 
+			
+			@Override
+			public void handle(ActionEvent actionEvent) {
+				if(animationRunning){
+					animation.getAnimation().stop();
+					for(AnimatedImageThread thr1 : animation.getThreadShapes())
+					{
+						thr1.getAnimation().stop();
+					}
+					animationRunning = false;
+					pauseAnimationButton.setText("Reprendre l'animation");
+				}
+				else {
+					animation.getAnimation().play();
+					for(AnimatedImageThread thr1 : animation.getThreadShapes())
+					{
+						thr1.getAnimation().play();
+					}
+					animationRunning = true;
+					pauseAnimationButton.setText("Mettre en pause l'animation");
+				}
+			}
+		});
+		
+		
+		Button snapshotAnimationButton = new Button();
+		snapshotAnimationButton.setText("Capturer l'animation en cours");
+		
+		snapshotAnimationButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	/* Save the capture in a png file */
+            	saveAsPngInResFolder();
+            	try {
+            		/* Open a new window with the capture in background */
+            		Stage secondaryStage = new Stage();
+					CatchWindowImpl catchAnimationWindow = new CatchWindowImpl(secondaryStage);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            }
+        });
+		
+		/* Add of the controllers to the VBox */
+		parametersVBox.getChildren().add(LabelNBMinObjects);
+		parametersVBox.getChildren().add(NbMinObjects);
+		
+		parametersVBox.getChildren().add(LabelTinyObjectsPercentage);
+		parametersVBox.getChildren().add(TinyObjectsPercentage);
+		
+		parametersVBox.getChildren().add(LabelNormalObjectsPercentage);
+		parametersVBox.getChildren().add(NormalObjectsPercentage);
+		
+		parametersVBox.getChildren().add(LabelBigObjectsPercentage);
+		parametersVBox.getChildren().add(BigObjectsPercentage);
+		
+		parametersVBox.getChildren().add(launchAnimationButton);
+		
+		parametersVBox.getChildren().add(pauseAnimationButton);
+		
+		parametersVBox.getChildren().add(snapshotAnimationButton);
+		
+		return parametersVBox;
+	}
+	
+	/**
+	 * Put the capture of the animation in a res folder
+	 * @author Florian, Etanlink
+	 * TODO For now it's just possible to save one capture, (each new capture overwrite the previous capture)
+	 * it will be necessary to think to fix this.
+	 */
+	public void saveAsPngInResFolder() {
+	    WritableImage image = this.snapshotRectangle.snapshot(new SnapshotParameters(), null);
+
+	    /* Save the capture in a png file in a res folder */
+	    File file = new File("src3/res/CaptureAnimation.png");
+	    try {
+	        ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+	    }
+	    catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+	/**
+	 * Loads the MenuBar of the UI
+	 * @return MenuBar menubar
+	 * @throws IOException
+	 */
+	/*private MenuBar loadMenuBar() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(WindowImpl.class.getResource("view/MenuBar.fxml"));
+		MenuBar menubar = (MenuBar)loader.load();
+		return menubar;
+	}*/
+
+	@FXML
+	public void LancerAnimationHandler(){
+		this.animationRunning = true;
+        /* Get the different parameters specified by the controllers */
+		/*
+    	double getNbMinObjectsParameter = NbMinObjects.getValue();
+    	double getTinyObjectsPercentage = TinyObjectsPercentage.getValue();
+    	double getNormalObjectsPercentage = NormalObjectsPercentage.getValue();
+    	double getBigObjectsPercentage = BigObjectsPercentage.getValue();
+    	*/
+    	/* Add of the animation controllers */
+    	/* Launch the animation */
+    	
+    	//animation.animationWithParameters(getNbMinObjectsParameter, getTinyObjectsPercentage, getNormalObjectsPercentage, getBigObjectsPercentage);
+		this.animation.run();
+	}
+
+	@FXML
+	public void DragHandler1(){
+		int j = (int) this.Slider1.getValue();
+		this.textField1.setText(String.valueOf(j));
+		this.Slider1.setValue(j);
+	}
+	
+	@FXML
+	public void TextAreaHandler1(){
+		this.Slider1.setValue(Double.parseDouble(this.textField1.getText()));
+	}
+
+	@FXML
+	public void DragHandler2(){
+		int j = (int) this.Slider2.getValue();
+		this.textField2.setText(String.valueOf(j));
+		this.Slider2.setValue(j);
+	}
+	
+	@FXML
+	public void TextAreaHandler2(){
+		this.Slider2.setValue(Double.parseDouble(this.textField2.getText()));
+	}
+	@FXML
+	public void DragHandler3(){
+		int j = (int) this.Slider3.getValue();
+		this.textField3.setText(String.valueOf(j));
+		this.Slider3.setValue(j);
+	}
+	
+	@FXML
+	public void TextAreaHandler3(){
+		this.Slider3.setValue(Double.parseDouble(this.textField3.getText()));
+	}
+	@FXML
+	public void DragHandler4(){
+		int j = (int) this.Slider4.getValue();
+		this.textField4.setText(String.valueOf(j));
+		this.Slider4.setValue(j);
+	}
+	
+	@FXML
+	public void TextAreaHandler4(){
+		this.Slider4.setValue(Double.parseDouble(this.textField4.getText()));
+	}
+	
+	@SuppressWarnings("deprecation")
+	@FXML
+	public void CheckPourcentage(){
+		double result = this.Slider3.getValue()+this.Slider2.getValue()+this.Slider4.getValue();
+		if (result < this.Slider1.getValue() || result > this.Slider1.getValue()){
+			Dialogs.create()
+					.owner(this.primaryStage)
+					.title("Erreur")
+					.masthead(null)
+					.message("Le nombre de monades demandé est erroné")
+					.showInformation();
+		}
+		if (result == this.Slider1.getValue()){
+			Dialogs.create()
+			.owner(this.primaryStage)
+			.title("truc")
+			.masthead(null)
+			.message("C'est OK")
+			.showInformation();
+		}
+		
+	}
+}
